0dd6062e39d39c62fdd52a4248d0da98875fe913
Travis Offtermatt <travisty2@gmail.com>
2020-09-11 23:08:05 -0600

    Allowing the base game's code to take over for updates as it's known to run as a single thread once all workers are done. Adding AmbientSoundManager to interface directly with the SustainManager_Patch's dict

--- Source/SustainerManager.cs
+++ Source/SustainerManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using Verse;
 using Verse.Sound;
 
@@ -8,29 +9,28 @@ namespace RimThreaded
 {
     public class SustainerManager_Patch
     {
-        public static ConcurrentDictionary<Sustainer, Sustainer> allSustainersDict = new ConcurrentDictionary<Sustainer, Sustainer>();
-        public static ConcurrentStack<List<Sustainer>> sListStack = new ConcurrentStack<List<Sustainer>>();
+        public static ConcurrentDictionary<Sustainer, Sustainer> allSustainers = new ConcurrentDictionary<Sustainer, Sustainer>();
 
         public static bool get_AllSustainers(SustainerManager __instance, ref List<Sustainer> __result)
         {
-            __result = allSustainersDict.Values.ToList();
+            __result = allSustainers.Values.ToList();
             return false;
         }
 
         public static bool RegisterSustainer(SustainerManager __instance, Sustainer newSustainer)
         {
-            allSustainersDict.TryAdd(newSustainer, newSustainer);
+            allSustainers.TryAdd(newSustainer, newSustainer);
             return false;
         }
 
         public static bool DeregisterSustainer(SustainerManager __instance, Sustainer oldSustainer)
         {
-            allSustainersDict.TryRemove(oldSustainer, out _);
+            allSustainers.TryRemove(oldSustainer, out _);
             return false;
         }
         public static bool SustainerExists(SustainerManager __instance, ref bool __result, SoundDef def)
         {
-            foreach (var kv in allSustainersDict)
+            foreach (var kv in allSustainers)
             {
                 if (kv.Value.def == def)
                 {
@@ -42,74 +42,26 @@ namespace RimThreaded
             return false;
         }
 
-        public static bool SustainerManagerUpdate(SustainerManager __instance)
+        private readonly static FieldInfo playingPerDefFI = typeof(SustainerManager).GetField("playingPerDef", BindingFlags.Static | BindingFlags.NonPublic);
+        public static void SustainerManagerUpdate(SustainerManager __instance)
         {
-            foreach (var kv in allSustainersDict)
+            // SustainerManagerUpdate is executed prior to Ticks so this will only ever be run on a single thread.
+            Dictionary<SoundDef, List<Sustainer>> d = playingPerDefFI.GetValue(__instance) as Dictionary<SoundDef, List<Sustainer>>;
+            d.Clear();
+            foreach (var kv in allSustainers)
             {
-                kv.Value.SustainerUpdate();
+                if (!d.TryGetValue(kv.Value.def, out List<Sustainer> l))
+                {
+                    l = new List<Sustainer>();
+                    d[kv.Value.def] = l;
+                }
+                l.Add(kv.Value);
             }
-            __instance.UpdateAllSustainerScopes();
-            return false;
         }
 
-        public static bool UpdateAllSustainerScopes(SustainerManager __instance)
-        {            
-            Dictionary<SoundDef, List<Sustainer>> playingPerDef = new Dictionary<SoundDef, List<Sustainer>>();
-            foreach (var kv in allSustainersDict)
-            {
-                Sustainer allSustainer = kv.Value;
-                if (!playingPerDef.ContainsKey(allSustainer.def))
-                {
-                    if (!sListStack.TryPop(out List<Sustainer> sustainerList))
-                        sustainerList = new List<Sustainer>();
-                    sustainerList.Add(allSustainer);
-                    playingPerDef.Add(allSustainer.def, sustainerList);
-                }
-                else
-                    playingPerDef[allSustainer.def].Add(allSustainer);
-            }
-            foreach (KeyValuePair<SoundDef, List<Sustainer>> keyValuePair in playingPerDef)
-            {
-                SoundDef key = keyValuePair.Key;
-                List<Sustainer> sustainerList = keyValuePair.Value;
-                if (sustainerList.Count - key.maxVoices < 0)
-                {
-                    for (int index = 0; index < sustainerList.Count; ++index)
-                        sustainerList[index].scopeFader.inScope = true;
-                }
-                else
-                {
-                    for (int index = 0; index < sustainerList.Count; ++index)
-                        sustainerList[index].scopeFader.inScope = false;
-                    sustainerList.Sort((a, b) => a.CameraDistanceSquared.CompareTo(b.CameraDistanceSquared));
-                    int num = 0;
-                    for (int index = 0; index < sustainerList.Count; ++index)
-                    {
-                        sustainerList[index].scopeFader.inScope = true;
-                        ++num;
-                        if (num >= key.maxVoices)
-                            break;
-                    }
-                    for (int index = 0; index < sustainerList.Count; ++index)
-                    {
-                        if (!sustainerList[index].scopeFader.inScope)
-                            sustainerList[index].scopeFader.inScopePercent = 0.0f;
-                    }
-                }
-            }
-            foreach (KeyValuePair<SoundDef, List<Sustainer>> keyValuePair in playingPerDef)
-            {
-                keyValuePair.Value.Clear();
-                //SimplePool<List<Sustainer>>.Return(keyValuePair.Value);
-                sListStack.Push(keyValuePair.Value);
-            }
-            //playingPerDef().Clear();
-            
-            return false;
-        }
         public static bool EndAllInMap(SustainerManager __instance, Map map)
         {
-            foreach (var kv in allSustainersDict)
+            foreach (var kv in allSustainers)
             {
                 if (kv.Value.info.Maker.Map == map)
                     kv.Value.End();
