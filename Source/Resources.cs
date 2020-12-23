98a46914149f2f7567714d8a036dfe3f02e459ed
JoJo1541 <jojo1541@gmx.de>
2020-12-23 04:56:50 +0100

    Fixed CTD from unity call
    Fixed CTD from unity call Resources.Load.
    
    Replaced Call to Resources.Load<Texture2D>(String path) to a safe imlementation.

---
+++ Source/Resources.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace RimThreaded
+{
+    class Resources_Patch
+    {
+        public static Func<object[], UnityEngine.Object> safeFunction = p => Resources.Load((string)p[0], (Type)p[1]);
+
+        public static UnityEngine.Object Load(string path, Type type)
+        {
+            int tID = Thread.CurrentThread.ManagedThreadId;
+            if (RimThreaded.mainRequestWaits.TryGetValue(tID, out EventWaitHandle eventWaitStart))
+            {
+                object[] functionAndParameters = new object[] { safeFunction, new object[] { path, type } };
+                lock (RimThreaded.safeFunctionRequests)
+                {
+                    RimThreaded.safeFunctionRequests[tID] = functionAndParameters;
+                }
+                RimThreaded.mainThreadWaitHandle.Set();
+                eventWaitStart.WaitOne();
+                RimThreaded.safeFunctionResults.TryGetValue(tID, out object safeFunctionResult);
+                return (UnityEngine.Object) safeFunctionResult;
+            }
+            Log.Error("Could not load Resource of type " + type.ToString() + " at path " + path);
+            return null;
+        }
+    }
+
+}
