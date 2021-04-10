fb8ebd039ff0868e7f65f9369bf97c5c585b42d9
Caleb Seelhoff <cseelhoff@gmail.com>
2021-04-10 15:44:20 -0400

    fixed thing spawn

--- Source/ThingGrid.cs
+++ Source/ThingGrid.cs
@@ -11,9 +11,22 @@ namespace RimThreaded
     {
         public static FieldRef<ThingGrid, Map> map = FieldRefAccess<ThingGrid, Map>("map");
         public static FieldRef<ThingGrid, List<Thing>[]> thingGrid = FieldRefAccess<ThingGrid, List<Thing>[]>("thingGrid");
-        public static Dictionary<Map, Dictionary<WorkGiver_Scanner, Dictionary<float, List<HashSet<Thing>[]>>>> mapIngredientDict = new Dictionary<Map, Dictionary<WorkGiver_Scanner, Dictionary<float, List<HashSet<Thing>[]>>>>();
+        public static Dictionary<Map, Dictionary<WorkGiver_Scanner, Dictionary<float, List<HashSet<Thing>[]>>>> mapIngredientDict =
+            new Dictionary<Map, Dictionary<WorkGiver_Scanner, Dictionary<float, List<HashSet<Thing>[]>>>>();
+        // Map, Scanner, points, (jumbo cell zoom level, #0 item=zoom 2x2, #1 item=4x4), jumbo cell index converted from x,z coord, HashSet<Thing>
         public static Dictionary<ThingDef, Dictionary<WorkGiver_Scanner, float>> thingBillPoints = new Dictionary<ThingDef, Dictionary<WorkGiver_Scanner, float>>();
-        public static int[] power2array = new int[] { 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384 }; // a 16000x16000 map is probably too big
+        // where is the list of WorkGiver_Scanners?
+        // loop through all WorkGiver_Scanners on map and add ThingDefs on map points
+        public static int[] power2array = new int[] { 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384 }; // a 16384x16384 map is probably too big
+
+        private static int CellToIndexCustom(IntVec3 c, int mapSizeX, int cellSize)
+        {
+            return (c.z * mapSizeX + c.x) / cellSize;
+        }
+        private static int NumGridCellsCustom(int mapSizeX, int mapSizeZ, int cellSize)
+        {
+            return Mathf.CeilToInt((mapSizeX * mapSizeZ) / (float)cellSize);
+        }
 
         public static void RunDestructivePatches()
         {
@@ -22,18 +35,10 @@ namespace RimThreaded
             RimThreadedHarmony.Prefix(original, patched, "RegisterInCell");
             RimThreadedHarmony.Prefix(original, patched, "DeregisterInCell");
         }
-        public static int CellToIndexCustom(IntVec3 c, int mapSizeX, int cellSize)
-        {
-            return (c.z * mapSizeX + c.x) / cellSize;
-        }
-        public static int NumGridCellsCustom(int mapSizeX, int mapSizeZ, int cellSize)
-        {
-            return Mathf.CeilToInt((mapSizeX * mapSizeZ) / (float)cellSize);
-        }
+
         public static bool RegisterInCell(ThingGrid __instance, Thing t, IntVec3 c)
         {
             Map this_map = map(__instance);
-            int i;
             if (!c.InBounds(this_map))
             {
                 Log.Warning(t.ToString() + " tried to register out of bounds at " + c + ". Destroying.", false);
@@ -41,36 +46,27 @@ namespace RimThreaded
             }
             else
             {
+                int index = this_map.cellIndices.CellToIndex(c);
+
                 int mapSizeX = this_map.Size.x;
                 int mapSizeZ = this_map.Size.z;
 
-                int index = this_map.cellIndices.CellToIndex(c);
-                Dictionary<WorkGiver_Scanner, float> billPointsDict = thingBillPoints[t.def];
-                Dictionary<WorkGiver_Scanner, Dictionary<float, List<HashSet<Thing>[]>>> ingredientDict = mapIngredientDict[this_map];
-                /*        
-                    if (!uniqueBillDict.Value.TryGetValue(points, out List<HashSet<Thing>[]> jumboCellsList))
-                    {
-                        jumboCellsList = new List<HashSet<Thing>[]>();
-                        i = 0;
-                        while (true)
-                        {
-                            int power2 = power2array[i];
-                            jumboCellsList.Add(new HashSet<Thing>[NumGridCellsCustom(mapSizeX, mapSizeZ, power2)]);
-                            if (power2 >= this_map.Size.x && power2 >= this_map.Size.z)
-                            {
-                                break;
-                            }
-                        }
-                        uniqueBillDict.Value.Add(points, jumboCellsList);
-                    }
-                    ingredientDict[billPoints.Key][billPoints.Value];
-                */
                 lock (__instance)
                 {
                     thingGrid(__instance)[index].Add(t);
+                    if (!thingBillPoints.TryGetValue(t.def, out Dictionary<WorkGiver_Scanner, float> billPointsDict))
+                    {
+                        billPointsDict = new Dictionary<WorkGiver_Scanner, float>();
+                        thingBillPoints[t.def] = billPointsDict;
+                    }
+                    if (!mapIngredientDict.TryGetValue(this_map, out Dictionary<WorkGiver_Scanner, Dictionary<float, List<HashSet<Thing>[]>>> ingredientDict))
+                    {
+                        ingredientDict = new Dictionary<WorkGiver_Scanner, Dictionary<float, List<HashSet<Thing>[]>>>();
+                        mapIngredientDict[this_map] = ingredientDict;
+                    }
                     foreach (KeyValuePair<WorkGiver_Scanner, float> billPoints in billPointsDict)
                     {
-                        i = 0;
+                        int i = 0;
                         int power2;
                         do
                         {
@@ -78,7 +74,7 @@ namespace RimThreaded
                             ingredientDict[billPoints.Key][billPoints.Value][i][CellToIndexCustom(c, mapSizeX, power2)].Add(t);
                             i++;
                         } while (power2 < mapSizeX || power2 < mapSizeZ);
-                    }                   
+                    }
                 }
             }
             return false;
@@ -106,6 +102,34 @@ namespace RimThreaded
                         List<Thing> newThingList = new List<Thing>(thingList);
                         newThingList.Remove(t);
                         thingGridInstance[index] = newThingList;
+
+                        int mapSizeX = this_map.Size.x;
+                        int mapSizeZ = this_map.Size.z;
+
+                        if (!thingBillPoints.TryGetValue(t.def, out Dictionary<WorkGiver_Scanner, float> billPointsDict))
+                        {
+                            billPointsDict = new Dictionary<WorkGiver_Scanner, float>();
+                            thingBillPoints[t.def] = billPointsDict;
+                        }
+                        if (!mapIngredientDict.TryGetValue(this_map, out Dictionary<WorkGiver_Scanner, Dictionary<float, List<HashSet<Thing>[]>>> ingredientDict))
+                        {
+                            ingredientDict = new Dictionary<WorkGiver_Scanner, Dictionary<float, List<HashSet<Thing>[]>>>();
+                            mapIngredientDict[this_map] = ingredientDict;
+                        }
+                        foreach (KeyValuePair<WorkGiver_Scanner, float> billPoints in billPointsDict)
+                        {
+                            int i = 0;
+                            int power2;
+                            do
+                            {
+                                power2 = power2array[i];
+                                HashSet<Thing> newHashSet = new HashSet<Thing>(ingredientDict[billPoints.Key][billPoints.Value][i][CellToIndexCustom(c, mapSizeX, power2)]);
+                                newHashSet.Remove(t);
+                                ingredientDict[billPoints.Key][billPoints.Value][i][CellToIndexCustom(c, mapSizeX, power2)] = newHashSet;
+                                i++;
+                            } while (power2 < mapSizeX || power2 < mapSizeZ);
+                        }
+
                     }
                 }
             }
