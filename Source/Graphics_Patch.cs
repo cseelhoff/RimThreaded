0a2e8d5c8072d618e04fd51b5082747787311780
JoJo1541 <jojo1541@gmx.de>
2021-06-09 04:15:05 +0200

    CTD HotSprings - GenDraw and Graphics
    CTD Fix for Hot Springs standalone and possibly Kijin Race

--- Source/Graphics_Patch.cs
+++ Source/Graphics_Patch.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using Verse;
 using static RimThreaded.RimThreaded;
 using static System.Threading.Thread;
 
@@ -7,9 +8,8 @@ namespace RimThreaded
 {
 
     public class Graphics_Patch
-	{
-        static readonly Action<object[]> ActionGraphicsBlit = p =>
-            Graphics.Blit((Texture)p[0], (RenderTexture)p[1]);
+    {
+        
 
         internal static void RunDestructivePatches()
         {
@@ -17,16 +17,23 @@ namespace RimThreaded
             Type patched = typeof(Graphics_Patch);
             RimThreadedHarmony.Prefix(original, patched, "Blit", new Type[] { typeof(Texture), typeof(RenderTexture) });
             RimThreadedHarmony.Prefix(original, patched, "DrawMesh", new Type[] { typeof(Mesh), typeof(Vector3), typeof(Quaternion), typeof(Material), typeof(int) });
+            RimThreadedHarmony.Prefix(original, patched, "DrawMeshNow", new Type[] { typeof(Mesh), typeof(Vector3), typeof(Quaternion) });
         }
+
+
+        static readonly Action<object[]> ActionGraphicsBlit = p =>
+            Graphics.Blit((Texture)p[0], (RenderTexture)p[1]);
+
         public static bool Blit(Texture source, RenderTexture dest)
         {
-            if (!CurrentThread.IsBackground || !allWorkerThreads.TryGetValue(CurrentThread, out ThreadInfo threadInfo)) 
+            if (!CurrentThread.IsBackground || !allWorkerThreads.TryGetValue(CurrentThread, out ThreadInfo threadInfo))
                 return true;
             threadInfo.safeFunctionRequest = new object[] { ActionGraphicsBlit, new object[] { source, dest } };
             mainThreadWaitHandle.Set();
             threadInfo.eventWaitStart.WaitOne();
             return false;
         }
+
 
         static readonly Action<object[]> ActionGraphicsDrawMesh = p =>
         Graphics.DrawMesh((Mesh)p[0], (Vector3)p[1], (Quaternion)p[2], (Material)p[3], (int)p[4]);
@@ -41,5 +48,19 @@ namespace RimThreaded
             return false;
         }
 
+
+        static readonly Action<object[]> ActionGraphicsDrawMeshNow = p =>
+        Graphics.DrawMeshNow((Mesh)p[0], (Vector3)p[1], (Quaternion)p[2]);
+
+        public static bool DrawMeshNow(Mesh mesh, Vector3 position, Quaternion rotation)
+        {
+            if (!CurrentThread.IsBackground || !allWorkerThreads.TryGetValue(CurrentThread, out ThreadInfo threadInfo))
+                return true;
+
+            threadInfo.safeFunctionRequest = new object[] { ActionGraphicsDrawMeshNow, new object[] { mesh, position, rotation } };
+            mainThreadWaitHandle.Set();
+            threadInfo.eventWaitStart.WaitOne();
+            return false;
+        }
     }
 }
