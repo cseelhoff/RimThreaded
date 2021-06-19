9e66baffa10138d2871ba658733346c7bb53098b
Caleb Seelhoff <cseelhoff@gmail.com>
2021-06-19 16:38:06 -0400

    Optimized Performance for Harvesting Job

--- Source/PlantHarvest_Cache.cs
+++ Source/PlantHarvest_Cache.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Verse;
 using static RimThreaded.Area_Patch;
+using static RimThreaded.JumboCell;
 
 namespace RimThreaded
 {
