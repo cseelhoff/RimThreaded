716454e97b418b8b50100a8438ae2f642629b754
Caleb Seelhoff <cseelhoff@gmail.com>
2021-08-11 18:36:27 -0400

    improved configure script

--- configure.ps1
+++ configure.ps1
@@ -1,8 +1,10 @@
-$rimworldSteamDir = "C:\Rimworld"
+$rimworldSourceFolder = "C:\RWSource"
+$rimworldSearchDir = "C:\Rimworld"
+
 if(Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 294100") {
-    $rimworldSteamDir = Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 294100" -Name InstallLocation
+    $rimworldSearchDir = Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 294100" -Name InstallLocation
 }
-if (!($rimworldDir = Read-Host "Enter RimWorld directory [$rimworldSteamDir]")) { $rimworldDir = $rimworldSteamDir }
+if (!($rimworldDir = Read-Host "Enter RimWorld directory [$rimworldSearchDir]")) { $rimworldDir = $rimworldSearchDir }
 $rimworldDataDir = Join-Path -Path $rimworldDir -ChildPath "RimWorldWin64_Data"
 $rimworldManagedDir = Join-Path -Path $rimworldDataDir -ChildPath "Managed"
 $assemblyPath = Join-Path -Path $rimworldManagedDir -ChildPath "Assembly-CSharp.dll"
@@ -11,27 +13,32 @@ if((Test-Path $assemblyPath) -eq $false) {
     Write-Error ("Assembly file not found in: " + $assemblyPath)
     exit
 }
-
 Write-Host ("Assembly file found at: " + $assemblyPath)
-$devFolder = "C:\Dev"
-$rimworldFolder = Join-Path -Path $devFolder -ChildPath "RimWorld"
-$rimworldTempFolder = Join-Path -Path $rimworldFolder -ChildPath "Temp"
-if((Test-Path -Path $rimworldTempFolder) -eq $false) {
-    New-Item -Path $rimworldTempFolder -ItemType Directory
-}
-dotnet tool install --global ilspycmd --version 7.1.0.6543
-Start-Process "ilspycmd" -ArgumentList @($assemblyPath, "-o", $rimworldTempFolder, "-p")
-Start-Process "ilspycmd" -ArgumentList @($assemblyPath, "-o", $rimworldTempFolder, "-genpdb") -Wait
-
-$pdbSourceFile = Join-Path -Path $rimworldTempFolder -ChildPath "Assembly-CSharp.pdb"
-$pdbDestinationFile = Join-Path -Path $rimworldManagedDir -ChildPath "Assembly-CSharp.pdb"
-Copy-Item -Path $pdbSourceFile -Destination $pdbDestinationFile
 
 $productVersion = Get-ChildItem -Path $assemblyPath | Select-Object -ExpandProperty VersionInfo | Select-Object -ExpandProperty ProductVersion
 $majorMinorSplit = $productVersion -split "\."
 $majorMinorVersion = $majorMinorSplit[0..1] -join "."
-
 Write-Host ("Detected Version: " + $majorMinorVersion)
+
+$rimworldSourceVersionFolder = Join-Path -Path $rimworldSourceFolder -ChildPath $majorMinorVersion
+if((Test-Path -Path $rimworldSourceVersionFolder) -eq $false) {
+    $null = New-Item -Path $rimworldSourceVersionFolder -ItemType Directory
+}
+
 $dependenciesDir = Convert-Path -Path ".\Dependencies"
 $dependencyVersionDir = Join-Path $dependenciesDir -ChildPath $majorMinorVersion
+Write-Host ("Copying: " + $assemblyPath + " to: " + $dependencyVersionDir)
 Copy-Item -Path $assemblyPath -Destination $dependencyVersionDir
+
+$ICSharpCodeDecompilerZip = Join-Path -Path $rimworldSourceFolder -ChildPath "ICSharpCode.Decompiler.zip"
+Invoke-WebRequest -Uri https://github.com/cseelhoff/ILSpy/releases/download/7.1b/ICSharpCode.Decompiler.zip -OutFile $ICSharpCodeDecompilerZip
+Expand-Archive -Path $ICSharpCodeDecompilerZip -DestinationPath $rimworldSourceFolder -Force
+$null = [Reflection.Assembly]::LoadFile((Join-Path -Path $rimworldSourceFolder -ChildPath "System.Reflection.Metadata.dll"))
+$null = [Reflection.Assembly]::LoadFile((Join-Path -Path $rimworldSourceFolder -ChildPath "System.Collections.Immutable.dll"))
+$null = [Reflection.Assembly]::LoadFile((Join-Path -Path $rimworldSourceFolder -ChildPath "ICSharpCode.Decompiler.dll"))
+$pdbDestinationFile = Join-Path -Path $rimworldManagedDir -ChildPath "Assembly-CSharp.pdb"
+Write-Host ("Decompiling source to: " + $rimworldSourceVersionFolder)
+Write-Host ("Writing PDB to: " + $pdbDestinationFile)
+Write-Host ("This process could take several minutes. Please wait...")
+[ICSharpCode.Decompiler.DebugInfo.PortablePdbWriter]::WritePdb($assemblyPath, $pdbDestinationFile, $rimworldSourceVersionFolder)
+
