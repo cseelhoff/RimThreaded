53451bee15c3ae2fd7a8e5fe86b85947dd2d5c3a
Sernior <56980041+Sernior@users.noreply.github.com>
2021-08-26 21:52:22 +0200

    District was missing a line.

--- Source/District_Patch.cs
+++ Source/District_Patch.cs
@@ -61,6 +61,7 @@ namespace RimThreaded
                 __instance.cachedCellCount = -1;
                 __instance.cachedOpenRoofCount = -1;
                 __instance.cachedOpenRoofState = null;
+                AnimalPenConnectedDistrictsCalculator.InvalidateDistrictCache(__instance);
                 __instance.lastChangeTick = Find.TickManager.TicksGame;
                 FacilitiesUtility.NotifyFacilitiesAboutChangedLOSBlockers(__instance.regions);
             }
