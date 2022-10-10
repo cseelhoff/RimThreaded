2783a589ac1dfe308b3c34e33164103f9745c027
Caleb Seelhoff <cseelhoff@gmail.com>
2022-10-09 21:58:00 -0500

    regiongrid_patch 2

--- Source/RW_Patches/RegionGrid_Patch.cs
+++ Source/RW_Patches/RegionGrid_Patch.cs
@@ -57,7 +57,7 @@ namespace RimThreaded.RW_Patches
                 Log.Warning(string.Concat("Trying to get valid region at ", c, " but RegionAndRoomUpdater is disabled. The result may be incorrect."));
             }
             Region region = __instance.regionGrid[map.cellIndices.CellToIndex(c)];
-            if (map.AgeInDays == 0f && region == null || !region.valid) //credit to BlackJack
+            if ((region == null || !region.valid) && Find.TickManager.TicksGame - map.generationTick == 0) //credit to BlackJack
             {
                 //not locking this breaks the generation of raid maps, since structure, pawn and loot generation depend on the canReachMapBorder or something in order to generate
                 //Log.Message("Intitially generating region for a map, namedly: " + map.uniqueID + " Dict :"+Regen);
